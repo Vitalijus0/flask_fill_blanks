d7dd056ad1facb7e1a9a62f3a90d4854fdbd632c
Vitalijus0 <vitalijus0612@gmail.com>
2021-06-27 18:47:21 +0300

    added Procfile

---
+++ schema.sql
@@ -0,0 +1,21 @@
+create table users (
+    id integer primary key autoincrement,
+    name text not null,
+    password text not null,
+    expert boolean not null,
+    admin boolean not null 
+);
+
+create table questions (
+    id integer primary key autoincrement,
+    question_text text not null,
+    answer_text text,
+    asked_by_id integer not null,
+    expert_id integer not null
+);
+
+create table comments (
+    id integer primary key autoincrement,
+    comment_text text not null,
+    commented_by_id integer not null,
+);
